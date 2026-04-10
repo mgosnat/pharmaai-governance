54792904216b3d63e623d27c77e4c7a7b2e1ebf8
Muriel Gosnat <muriel@gosnat.com>
2026-04-10 09:52:41 +0200

    feat: FR toggle + PAR landing

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import { Routes, Route } from 'react-router-dom'
+import GovernancePage from './pages/GovernancePage'
+export default function App() {
+  return <Routes><Route path="*" element={<GovernancePage />} /></Routes>
+}
